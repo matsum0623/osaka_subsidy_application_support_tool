55df2f3bd601f3d87af57b621db5f5d471300af9
m.matsuura <matsum0623@gmail.com>
2024-07-01 00:16:42 +0900

    共通処理用のレイヤー作成

--- apigateway/monthly/daily/get/index.js
+++ apigateway/monthly/daily/get/index.js
@@ -1,17 +1,14 @@
 const AWS = require('aws-sdk')
 const dynamo = new AWS.DynamoDB.DocumentClient({ region: 'localhost', endpoint: 'http://host.docker.internal:8000' });
 
+const {response_ok, response_400} = require('lambda_response')
+const {daily, instructor} = require('connect_dynamodb')
+
 exports.handler = async (event, context) => {
   const today = new Date()
   const qsp = event.queryStringParameters
   if (!qsp.date){
-    return  {
-      statusCode: 400,
-      headers: {
-        'x-custom-header': 'my custom header value',
-        "Access-Control-Allow-Origin": "*"
-      }
-    }
+    return response_400
   }
 
   // その日の情報を取得
@@ -28,38 +25,26 @@ exports.handler = async (event, context) => {
   }
   const instructor_data = {}
   try {
-      const result = await dynamo.query({
-          TableName: 'test-table',
-          KeyConditionExpression: 'PK = :p_key AND SK = :s_key',
-          ExpressionAttributeValues: {
-              ':p_key': 'AFTER_SCHOOL#0001',
-              ':s_key': 'DAILY#' + qsp.date,
-          },
-      }).promise();
-      result.Items.forEach(item => {
-        res_data['children'] = {
-          'sum': item.Children,
-          'disability': item.Disability,
-          'medical_care': item.MedicalCare,
-        }
-        res_data['summary']['hours'] = item.Details.Summary.WorkHours
-        item.Details.InstructorWorkHours.forEach((ins) => {
-          instructor_data[ins.InstructorId] = ins
-        })
+    const result = await daily.get_item('0001', qsp.date)
+
+    result.Items.forEach(item => {
+      res_data['children'] = {
+        'sum': item.Children,
+        'disability': item.Disability,
+        'medical_care': item.MedicalCare,
+      }
+      res_data['summary']['hours'] = item.Details.Summary.WorkHours
+      item.Details.InstructorWorkHours.forEach((ins) => {
+        instructor_data[ins.InstructorId] = ins
+      })
     });
   } catch (error) {
       console.log(error.message)
   }
   // 指導員情報を取得
   try {
-    const result = await dynamo.query({
-        TableName: 'test-table',
-        KeyConditionExpression: 'PK = :p_key AND begins_with(SK, :s_key)',
-        ExpressionAttributeValues: {
-            ':p_key': 'AFTER_SCHOOL#0001',
-            ':s_key': 'INSTRUCTOR#',
-        },
-    }).promise();
+    const result = await instructor.get_all('0001')
+
     // 結果を日付をキーにしたオブジェクトに変換
     result.Items.forEach(item => {
       const instructor_id = item.SK.substring(11)
@@ -75,16 +60,5 @@ exports.handler = async (event, context) => {
       console.log(error.message)
   }
 
-  const response = {
-    statusCode: 200, // HTTP 200 OK
-    headers: {
-      'x-custom-header': 'my custom header value',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      data: res_data,
-    })
-  };
-
-  return response;
+  return response_ok(res_data)
 };
